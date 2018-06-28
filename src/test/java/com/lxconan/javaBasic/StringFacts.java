851833cf4ee51182e3e9040d09d7808cfbe35ecb
Liu Xia <lxconan@gmail.com>
2018-06-28 10:42:17 +0800

    [liuxia] Add string related and regex related facts.

--- src/test/java/com/lxconan/javaBasic/StringFacts.java
+++ src/test/java/com/lxconan/javaBasic/StringFacts.java
@@ -1,8 +1,13 @@
 package com.lxconan.javaBasic;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class StringFacts {
 
@@ -85,4 +90,108 @@ public class StringFacts {
      * - What will happen if the the starting index is greater than the ending index?
      * - What will happen if the input string is of null reference?
      */
+
+    @SuppressWarnings({"unused", "ConstantConditions"})
+    @Test
+    public void should_break_string_into_words() {
+        final String sentence = "This is Mike";
+
+        // TODO: Extract words in the sentence.
+        // <--Start
+        String[] words = null;
+        // --End-->
+
+        assertArrayEquals(new String[] {"This", "is", "Mike"}, words);
+    }
+
+    @SuppressWarnings({"unused", "ConstantConditions"})
+    @Test
+    public void should_break_string_into_words_customized() {
+        final String sentence = "This/is/Mike";
+
+        // TODO: Extract words in the sentence.
+        // <--Start
+        String[] words = null;
+        // --End-->
+
+        assertArrayEquals(new String[] {"This", "is", "Mike"}, words);
+    }
+
+    @SuppressWarnings({"unused", "StringBufferReplaceableByString", "MismatchedQueryAndUpdateOfStringBuilder"})
+    @Test
+    public void should_create_ascii_art() {
+        final int width = 5;
+        final int height = 3;
+
+        // TODO: Create string using StringBuilder
+        // <--Start
+        StringBuilder builder = new StringBuilder();
+        // --End-->
+
+        final String expected =
+            "|---|\n" +
+            "|   |\n" +
+            "|---|\n";
+
+        assertEquals(expected, builder.toString());
+    }
+
+    @SuppressWarnings("unused")
+    @Test
+    public void should_calculate_checksum_of_a_string() {
+        final String text = "A quick brown fox jumps over a lazy dog.";
+
+        int sum = 0;
+        // TODO: Write some code to calculate the checksum of the string. The checksum is the sum of each string char.
+        // <--Start
+        // --End-->
+
+        Assert.assertEquals(3655, sum);
+    }
+
+    @Test
+    public void should_convert_unicode_escape() {
+        final String expected = "なにこれ";
+
+        // TODO: Write actual string using unicode escape. The unicode is as follows:
+        // な - U+306a
+        // に - U+306b
+        // こ - U+3053
+        // れ - U+308c
+        // <--Start
+        final String actual = "";
+        // --End-->
+
+        assertEquals(expected, actual);
+    }
+
+    @SuppressWarnings("unused")
+    @Test
+    public void should_reverse_a_string() {
+        final String original = "123456";
+
+        // TODO: Modify the following code to create new string from original String
+        // <--Start
+        final String reversed = "";
+        // --End-->
+
+        assertEquals("654321", reversed);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void should_compare_string_with_different_cases() {
+        final String uppercased = "HELLO";
+        final String lowercased = "hello";
+
+        Optional<Boolean> equalResult = Optional.of(uppercased.equals(lowercased));
+        Optional<Boolean> equalIgnoreCaseResult = Optional.of(uppercased.equalsIgnoreCase(lowercased));
+
+        // TODO: Please change the value of the following 2 lines to pass the test.
+        Optional<Boolean> actualResultOfEqual = Optional.empty();
+        Optional<Boolean> actualResultOfEqualIgnoreCase = Optional.empty();
+
+        assertEquals(equalResult, actualResultOfEqual);
+        assertEquals(equalIgnoreCaseResult, actualResultOfEqualIgnoreCase);
+    }
 }
