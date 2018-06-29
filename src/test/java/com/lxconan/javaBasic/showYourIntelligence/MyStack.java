61ecf933a6c9487aded297bfeb438975b1b8e4ec
Liu Xia <lxconan@gmail.com>
2018-06-29 17:18:32 +0800

    [liuxia] Add structuring data facts.

---
+++ src/test/java/com/lxconan/javaBasic/showYourIntelligence/MyStack.java
@@ -0,0 +1,57 @@
+package com.lxconan.javaBasic.showYourIntelligence;
+
+@SuppressWarnings({"unused", "FieldCanBeLocal"})
+public class MyStack {
+    private int[] storage;
+    private int capacity;
+    private int count;
+    private static final int GROW_FACTOR = 2;
+
+    public MyStack(int initialCapacity) {
+        if (initialCapacity < 1) {
+            throw new IllegalArgumentException("Capacity too small.");
+        }
+
+        capacity = initialCapacity;
+        storage = new int[initialCapacity];
+        count = 0;
+    }
+
+    public void push(int value) {
+        if (count == capacity) {
+            ensureCapacity();
+        }
+
+        // TODO: Please push the value into the storage here.
+        // <--start
+        // --end-->
+    }
+
+    private void ensureCapacity() {
+        int newCapacity = capacity * GROW_FACTOR;
+
+        // TODO: Please create a new array of size newCapacity. And update related fields
+        // TODO: You SHOULD NOT USE COLLECTIONS OTHER THAN ARRAY.
+        // <--start
+        // --end-->
+    }
+
+    public int[] popToArray() {
+        final int totalItemsCount = count;
+        int[] array = new int[totalItemsCount];
+
+        while (count > 0) {
+            array[totalItemsCount - count] = pop();
+        }
+
+        return array;
+    }
+
+    private int pop() {
+        // TODO: Please pop one element from the array.
+        // <--start
+        // --end-->
+
+        throw new UnsupportedOperationException("Stack is empty.");
+    }
+}
